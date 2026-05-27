85532ff0b5322d9de538a60e0cf9f2169fa4ca21
Harshvardhan <harshvardhanti12@gmail.com>
2026-05-27 13:09:50 +0530

    Fix: revert firebase projectId to match actual project

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,9 +3,10 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgPWcFzPb1WwXuGC-N7Dh_29xruHjAtug",
-  authDomain: "zenjob-db.firebaseapp.com",
-  projectId: "zenjob-db",
-  storageBucket: "zenjob-db.firebasestorage.app",
+  authDomain: "magiccounter-db.firebaseapp.com",
+  projectId: "magiccounter-db",
+  storageBucket: "magiccounter-db.firebasestorage.app",
+
 
   messagingSenderId: "150409536528",
   appId: "1:150409536528:web:91e38f15e16a0bc28d5695",
